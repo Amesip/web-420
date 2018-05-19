5a08ac3b64b3bf0cbd968d5fb9bbb879caac5f57
Mary Barrett <mkbarrett.1201@gmail.com>
2018-05-19 13:23:50 -0500

    Adding Week 4

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -5,7 +5,15 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({ username: String,
 password: String, email: String
 });
+module.exports.add = (user, callback) => {
+    user.save(callback);
+}
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+}
 module.exports = mongoose.model('User', userSchema);
+
 /**
 Database queries
 */
